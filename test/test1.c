37a435e2d8d1836711a194e1b3c1722622af46ff
SakibFuad <sfuad001@ucr.edu>
2023-02-16 03:42:37 -0800

    initial commit

---
+++ test/test1.c
@@ -0,0 +1,11 @@
+void test() {
+	int a, b, c, e;
+	e = b + c;
+	if (e > 0) {
+		e = a;
+	} 
+	else {
+		a = b + c;
+	}
+	a = e + c;
+}
---
+++ test/test4.c
@@ -0,0 +1,13 @@
+void test(){
+	int a,b,c,e;
+	a=b=5;
+	c=0;
+	e=4;
+	for(int i=0;i<100;i++){
+		c=a-b;
+		while(i+e<20){
+			i=a+i;
+		}
+		b=c*a;
+	}
+}
